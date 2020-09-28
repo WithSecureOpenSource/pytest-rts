3b5ddadaf73a5fa7b942b6d5acdcbe1c4aea57ba
guotin <eero.kauhanen@helsinki.fi>
2020-09-28 12:17:08 +0300

    Fix tests for new structure and reconfigure tmp folder actions

--- tests_selector/tests/test_db.py
+++ tests_selector/tests/test_db.py
@@ -1,12 +1,11 @@
-import pytest
-import os
-import subprocess
+import sqlite3
+from tests_selector.utils.db import DatabaseHelper
 
-from tests_selector.utils import db
+db_name = "mapping.db"
 
-
-def test_delete_ran_lines(temp_project_repo):
-    c, conn = db.get_cursor()
+def test_delete_ran_lines():
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
     file_id = 1
     sql = "SELECT line_id FROM test_map WHERE file_id = ?"
     line_ids = [x[0] for x in c.execute(sql, (file_id,)).fetchall()]
@@ -14,38 +13,49 @@ def test_delete_ran_lines(temp_project_repo):
 
     assert line_ids == [4, 5, 6, 9, 12]
 
+    db = DatabaseHelper()
+    db.init_conn()
     db.delete_ran_lines(line_ids, file_id)
-    c, conn = db.get_cursor()
+    db.close_conn()
+
+
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
     line_ids = [x[0] for x in c.execute(sql, (file_id,)).fetchall()]
     conn.close()
 
     assert line_ids == []
-    subprocess.run(["tests_selector_init"])
 
 
-def test_update_db_from_src_mapping(temp_project_repo):
+def test_update_db_from_src_mapping():
     # shift all lines +5 and compare old and new lines in db
-    c, conn = db.get_cursor()
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
     file_id = 1
     sql = "SELECT line_id FROM test_map WHERE file_id = ?"
     line_ids = [x[0] for x in c.execute(sql, (file_id,)).fetchall()]
     conn.close()
 
     assert line_ids == [4, 5, 6, 9, 12]
-
     line_map = {x: x + 5 for x in line_ids}
+
+    db = DatabaseHelper()
+    db.init_conn()
     db.update_db_from_src_mapping(line_map, file_id)
-    c, conn = db.get_cursor()
+    db.close_conn()
+
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
     line_ids = [x[0] for x in c.execute(sql, (file_id,)).fetchall()]
     conn.close()
 
     assert line_ids == [9, 10, 11, 14, 17]
-    subprocess.run(["tests_selector_init"])
 
 
-def test_update_db_from_test_mapping(temp_project_repo):
+def test_update_db_from_test_mapping():
     # shift all lines +5 and compare old and new lines in db
-    c, conn = db.get_cursor()
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
     file_id = 2
     sql = "SELECT id,start,end FROM test_function WHERE test_file_id = ?"
     func_line_dict = {x[0]: (x[1], x[2]) for x in c.execute(sql, (file_id,)).fetchall()}
@@ -53,9 +63,14 @@ def test_update_db_from_test_mapping(temp_project_repo):
 
     line_count = sum(1 for line in open("tests/test_some_methods.py"))
     line_map = {x: x + 5 for x in range(1, line_count + 1)}
-    db.update_db_from_test_mapping(line_map, file_id)
 
-    c, conn = db.get_cursor()
+    db = DatabaseHelper()
+    db.init_conn()
+    db.update_db_from_test_mapping(line_map, file_id)
+    db.close_conn()
+
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
     func_line_dict2 = {
         x[0]: (x[1], x[2]) for x in c.execute(sql, (file_id,)).fetchall()
     }
@@ -67,5 +82,3 @@ def test_update_db_from_test_mapping(temp_project_repo):
         new_tuple = func_line_dict2[key]
         assert old_tuple[0] + 5 == new_tuple[0]
         assert old_tuple[1] + 5 == new_tuple[1]
-
-    subprocess.run(["tests_selector_init"])
