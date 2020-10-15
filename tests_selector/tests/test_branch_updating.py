e72491a5aeb44be3bb9c687d11c73a91961bd30a
guotin <42908140+guotin@users.noreply.github.com>
2020-10-15 11:55:04 +0300

    Add test for committed changes checking (#32)

--- tests_selector/tests/test_branch_updating.py
+++ tests_selector/tests/test_branch_updating.py
@@ -3,9 +3,11 @@ import os
 import subprocess
 import sqlite3
 
+from tests_selector.utils.db import DB_FILE_NAME, DatabaseHelper
+from tests_selector import select
+
 
 def test_branch_updating():
-    DB_FILE_NAME = "mapping.db"
     # get test_map lines for car.py
     conn = sqlite3.connect(DB_FILE_NAME)
     c = conn.cursor()
@@ -114,3 +116,56 @@ def test_branch_updating():
 
     # All car mapping lines should be old + 2 and not updated twice
     assert [x + 2 for x in old_line_ids_car] == new_line_ids_car
+
+
+@pytest.mark.parametrize(
+    "change_list,expected",
+    [
+        (
+            ["changes/car_01.txt", "changes/shop_01.txt", "changes/test_car_01.txt"],
+            {
+                "tests/test_some_methods.py::test_normal_shop_purchase2",
+                "tests/test_some_methods.py::test_normal_shop_purchase",
+                "tests/test_car.py::test_passengers",
+                "tests/test_car.py::test_acceleration",
+            },
+        )
+    ],
+)
+def test_skipping_commit(change_list, expected):
+    # Checkout a new branch
+    subprocess.run(["git", "checkout", "-b", "new-branch"])
+
+    # Change car.py from changes folder and commit changes
+    subprocess.run(["cp", "-f", change_list[0], "src/car.py"])
+    subprocess.run(["git", "add", "src/car.py"])
+    subprocess.run(["git", "commit", "-m", "commit1"])
+
+    # Run tests selector and db should update
+    subprocess.run(["tests_selector"])
+
+    # Change shop.py from changes folder and commit changes
+    subprocess.run(["cp", "-f", change_list[1], "src/shop.py"])
+    subprocess.run(["git", "add", "src/shop.py"])
+    subprocess.run(["git", "commit", "-m", "commit2"])
+
+    # Change test_car.py from changes folder and commit changes
+    subprocess.run(["cp", "-f", change_list[2], "tests/test_car.py"])
+    subprocess.run(["git", "add", "tests/test_car.py"])
+    subprocess.run(["git", "commit", "-m", "commit3"])
+
+    # Tests selectors functions should find tests from changes:
+    # Commit1 -> Commit2 AND Commit2 -> Commit3
+    # DB required to use the function
+    db = DatabaseHelper()
+    db.init_conn()
+    (
+        test_set,
+        update_tuple,
+        changed_test_files_amount,
+        changed_src_files_amount,
+        new_tests_amount,
+    ) = select.get_tests_and_data_committed(db)
+    db.close_conn()
+
+    assert test_set == expected
