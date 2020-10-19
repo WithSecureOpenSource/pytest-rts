6b84564e329eb7f3719a9dc5bcdde7a9e6dc5ed5
guotin <eero.kauhanen@helsinki.fi>
2020-10-19 12:15:00 +0300

    Refactor test_e2e

--- tests_selector/tests/test_e2e.py
+++ tests_selector/tests/test_e2e.py
@@ -1,274 +1,213 @@
 import subprocess
 import sqlite3
-from tests_selector.utils import (
-    common,
-    git,
+import pytest
+from tests_selector.utils.common import read_newly_added_tests
+from tests_selector.utils.db import DatabaseHelper, DB_FILE_NAME
+from tests_selector.select import (
+    get_tests_and_data_current,
+    get_tests_and_data_committed,
 )
-from tests_selector.utils.db import DatabaseHelper
-from tests_selector import select
-
-initial_mapping_name = "mapping.db"
 
 
-def test_full_integration():
-    subprocess.run(["git", "checkout", "-b", "new-branch"])
+def get_mapping_id_from_filename(filename, is_srcfile):
+    if is_srcfile:
+        sql = "SELECT id FROM src_file WHERE path = ?"
+    else:
+        sql = "SELECT id from test_file WHERE path = ?"
+
+    conn = sqlite3.connect(DB_FILE_NAME)
+    c = conn.cursor()
+    file_id = c.execute(sql, (filename,)).fetchone()[0]
+    conn.close()
+    return file_id
+
+
+def get_mapping_lines_for_srcfile(src_file_id):
+    conn = sqlite3.connect(DB_FILE_NAME)
+    c = conn.cursor()
+    lines = [
+        x[0]
+        for x in c.execute(
+            "SELECT line_id FROM test_map WHERE file_id = ?", (src_file_id,)
+        ).fetchall()
+    ]
+    conn.close()
+    return lines
+
+
+def get_tests_from_tool_current():
     db = DatabaseHelper()
     db.init_conn()
-
-    # Check correct files in mapping to begin with
-    test_files, src_files = db.get_testfiles_and_srcfiles()
-    car_id = [x[0] for x in src_files if x[1] == "src/car.py"][0]
-    src_file_names = [x[1] for x in src_files]
-    test_file_names = [x[1] for x in test_files]
-
-    assert src_file_names == ["src/car.py", "src/shop.py"]
-    assert test_file_names == ["tests/test_car.py", "tests/test_some_methods.py"]
-
-    # Add a new method to car.py
-    with open("./src/car.py", "a") as f:
-        f.write("\n\n")
-        f.write("    def new_method():\n")
-        f.write("        i = self.speed + self.seats\n")
-        f.write("        return i + 8\n")
-
-    # Get working directory test_set like in tests_selector script
-
     (
-        workdir_test_set,
-        workdir_changed_test_files,
-        workdir_changed_src_files,
-    ) = select.get_tests_and_data_current(db)
-
-    all_tests_car = db.query_all_tests_srcfile(car_id)
-
-    # Close conn of tool's db just in case
+        test_set,
+        _,
+        _,
+    ) = get_tests_and_data_current(db)
     db.close_conn()
+    return test_set
+
+
+def get_tests_from_tool_committed():
+    db = DatabaseHelper()
+    db.init_conn()
+    (
+        test_set,
+        _,
+        _,
+        _,
+        _,
+    ) = get_tests_and_data_committed(db)
+    db.close_conn()
+    return test_set
+
+
+def get_all_tests_for_srcfile(src_file_id):
+    db = DatabaseHelper()
+    db.init_conn()
+    all_tests = db.query_all_tests_srcfile(src_file_id)
+    db.close_conn()
+    return all_tests
+
+
+def get_newly_added_tests_from_tool():
+    db = DatabaseHelper()
+    db.init_conn()
+    new_tests = read_newly_added_tests(db)
+    db.close_conn()
+    return new_tests
+
+
+def new_test_exists_in_mapping_db(testname):
+    conn = sqlite3.connect(DB_FILE_NAME)
+    c = conn.cursor()
+    sql = "SELECT EXISTS(SELECT id FROM test_function WHERE context = ?)"
+    exists = bool(c.execute(sql, (testname,)).fetchone()[0])
+    return exists
+
+
+@pytest.mark.parametrize(
+    "new_method_srcfile, new_method_testfile",
+    [
+        (
+            ("changes/car/add_new_method.txt", "src/car.py"),
+            (
+                "changes/test_car/add_test_passengers.txt",
+                "tests/test_car.py",
+                "tests/test_car.py::test_passengers",
+            ),
+        ),
+    ],
+)
+def test_full_integration(new_method_srcfile, new_method_testfile):
+    subprocess.run(["git", "checkout", "-b", "new-branch"])
+
+    change_for_new_src_method = new_method_srcfile[0]
+    filename_for_new_src_method = new_method_srcfile[1]
+    src_file_id = get_mapping_id_from_filename(
+        filename_for_new_src_method, is_srcfile=True
+    )
+
+    change_for_new_test = new_method_testfile[0]
+    filename_for_new_test = new_method_testfile[1]
+    new_test_name = new_method_testfile[2]
+
+    # Add a new method to source file
+    subprocess.run(["cp", "-f", change_for_new_src_method, filename_for_new_src_method])
+
+    all_tests_srcfile = get_all_tests_for_srcfile(src_file_id)
+    # Get working directory test_set like in tests_selector script
+    workdir_test_set = get_tests_from_tool_current()
 
     # New method addition = test_set should be all tests of that file
-    assert workdir_test_set == set(all_tests_car)
-
-    # Open separate connection to database for this test
-    conn = sqlite3.connect(initial_mapping_name)
-    c = conn.cursor()
+    assert workdir_test_set == set(all_tests_srcfile)
 
     # Run tests_selector, db shouldn't update
-    old_car_lines = [
-        x[0]
-        for x in c.execute(
-            "SELECT line_id FROM test_map WHERE file_id = ?", (car_id,)
-        ).fetchall()
-    ]
+    old_srcfile_lines = get_mapping_lines_for_srcfile(src_file_id)
     subprocess.run(["tests_selector"])
-    new_car_lines = [
-        x[0]
-        for x in c.execute(
-            "SELECT line_id FROM test_map WHERE file_id = ?", (car_id,)
-        ).fetchall()
-    ]
-    assert old_car_lines == new_car_lines
+    new_srcfile_lines = get_mapping_lines_for_srcfile(src_file_id)
 
-    # Close test connection
-    conn.close()
+    assert old_srcfile_lines == new_srcfile_lines
 
     # Commit changes
-    subprocess.run(["git", "add", "src/car.py"])
-    subprocess.run(["git", "commit", "-m", "car_changes"])
-
-    # Reopen tool's db connection
-    db.init_conn()
+    subprocess.run(["git", "add", filename_for_new_src_method])
+    subprocess.run(["git", "commit", "-m", "new_method_src"])
 
     # Get committed changes test_set like in tests_selector script
-    (
-        commit_test_set,
-        commit_update_tuple,
-        commit_changed_test_files,
-        commit_changed_src_files,
-        new_tests_amount,
-    ) = select.get_tests_and_data_committed(db)
+    commit_test_set = get_tests_from_tool_committed()
+
     # New method addition = test_set should be all tests of that file
-    assert commit_test_set == set(all_tests_car)
+    assert commit_test_set == set(all_tests_srcfile)
 
-    # Close conn of tool's db just in case
-    db.close_conn()
-
-    # Open separate connection to database for this test
-    conn = sqlite3.connect(initial_mapping_name)
-    c = conn.cursor()
-
-    # DB should update after running this
+    # DB should update after running test selector
     # But no new test tests new method so lines should be the same
     subprocess.run(["tests_selector"])
-    new_car_lines = [
-        x[0]
-        for x in c.execute(
-            "SELECT line_id FROM test_map WHERE file_id = ?", (car_id,)
-        ).fetchall()
-    ]
-    assert old_car_lines == new_car_lines
-
-    # Close test connection
-    conn.close()
+    new_srcfile_lines = get_mapping_lines_for_srcfile(src_file_id)
+    assert old_srcfile_lines == new_srcfile_lines
 
     # Add a new test method
-    with open("./tests/test_car.py", "a") as f:
-        f.write("\n\n")
-        f.write("def test_add_passenger():\n")
-        f.write("    car = Car(3,0,1)\n")
-        f.write("    car.add_passenger()\n")
-        f.write("    assert car.get_passengers() == 2\n")
-
-    # Reopen tool's db connection
-    db.init_conn()
+    subprocess.run(["cp", "-f", change_for_new_test, filename_for_new_test])
 
     # Get working directory diffs and test_set like in tests_selector script
-    (
-        workdir_test_set2,
-        workdir_changed_test_files2,
-        workdir_changed_src_files2,
-    ) = select.get_tests_and_data_current(db)
-
-    # Close conn of tool's db just in case
-    db.close_conn()
-
+    workdir_test_set2 = get_tests_from_tool_current()
     # Changes test_set should be empty
     # But newline at the end existing test is also considered a change
-    assert workdir_test_set2 == set(["tests/test_car.py::test_acceleration"])
-
-    new_test_name = "tests/test_car.py::test_add_passenger"
-
-    # Open separate connection to database for this test
-    conn = sqlite3.connect(initial_mapping_name)
-    c = conn.cursor()
+    assert workdir_test_set2 == set()
 
     # Running test_selector should not add new test to database
     subprocess.run(["tests_selector"])
-    new_test_in_db = c.execute(
-        "SELECT id FROM test_function WHERE context = ?", (new_test_name,)
-    ).fetchall()
-    assert len(new_test_in_db) == 0
-
-    # Close test connection
-    conn.close()
+    assert not new_test_exists_in_mapping_db(new_test_name)
 
     # Commit changes
-    subprocess.run(["git", "add", "tests/test_car.py"])
-    subprocess.run(["git", "commit", "-m", "test_car_changes"])
-
-    # Reopen tool's db connection
-    db.init_conn()
+    subprocess.run(["git", "add", filename_for_new_test])
+    subprocess.run(["git", "commit", "-m", "new_test_method"])
 
     # Get committed changes test_set like in tests_selector script
-    (
-        commit_test_set2,
-        commit_update_tuple2,
-        commit_changed_test_files2,
-        commit_changed_src_files2,
-        new_tests_amount2,
-    ) = select.get_tests_and_data_committed(db)
+    commit_test_set2 = get_tests_from_tool_committed()
 
     # Test_set should now include all tests from changes between this commit and previous
     # = New test method + newline causing acceleration test to show up
-
-    assert commit_test_set2 == {"tests/test_car.py::test_acceleration", new_test_name}
+    assert commit_test_set2 == {new_test_name}
 
     # New tests should include the newly added test only
-    new_tests = common.read_newly_added_tests(db)
-    assert list(new_tests) == [new_test_name]
+    new_tests = get_newly_added_tests_from_tool()
+    assert new_tests == {new_test_name}
 
-    # Close conn of tool's db just in case
-    db.close_conn()
-
-    # Running tests_selector should now update database
-    # So new test function should be found in db
+    # Running tests_selector should now update database = new test function should be found in db
     subprocess.run(["tests_selector"])
-
-    # Open separate connection to database for this test
-    conn = sqlite3.connect(initial_mapping_name)
-    c = conn.cursor()
-
-    new_test_func_id = c.execute(
-        "SELECT id FROM test_function WHERE context = ?", (new_test_name,)
-    ).fetchone()
-    assert new_test_func_id[0] != None
-    new_mapping_lines = [
-        x[0]
-        for x in c.execute(
-            "SELECT line_id FROM test_map WHERE test_function_id = ? AND file_id = ?",
-            (new_test_func_id[0], car_id),
-        ).fetchall()
-    ]
-    assert new_mapping_lines == [4, 5, 6, 15, 18, 19]
-
-    conn.close()
+    assert new_test_exists_in_mapping_db(new_test_name)
 
 
-def test_db_updating_only_once():
+@pytest.mark.parametrize(
+    "change, new_mapping, filename",
+    [("changes/car/shift_2_forward.txt", [6, 7, 8, 11, 14], "src/car.py")],
+)
+def test_db_updating_only_once(change, new_mapping, filename):
     subprocess.run(["git", "checkout", "-b", "new-branch"])
 
-    conn = sqlite3.connect(initial_mapping_name)
-    c = conn.cursor()
-    file_id_car = c.execute(
-        "SELECT id FROM src_file WHERE path = ?", ("src/car.py",)
-    ).fetchone()[0]
-    old_car_lines = [
-        x[0]
-        for x in c.execute(
-            "SELECT line_id FROM test_map WHERE file_id = ?", (file_id_car,)
-        ).fetchall()
-    ]
-    conn.close()
+    src_file_id = get_mapping_id_from_filename(filename, is_srcfile=True)
+    old_lines = get_mapping_lines_for_srcfile(src_file_id)
 
-    with open("./src/car.py", "r") as f:
-        lines = f.readlines()
-        lines[4] = lines[4] + "\n\n\n"
-        lines[12] = lines[12] + "\n\n"
-
-    with open("./src/car.py", "w") as f:
-        for line in lines:
-            f.write(line)
+    # Change src file
+    subprocess.run(["cp", "-f", change, filename])
 
     # Changes in working directory, run tests_selector
     # Shouldn't update db
     subprocess.run(["tests_selector"])
-    conn = sqlite3.connect(initial_mapping_name)
-    c = conn.cursor()
-    new_car_lines = [
-        x[0]
-        for x in c.execute(
-            "SELECT line_id FROM test_map WHERE file_id = ?", (file_id_car,)
-        ).fetchall()
-    ]
-    conn.close()
-    assert old_car_lines == new_car_lines
+    new_lines = get_mapping_lines_for_srcfile(src_file_id)
+    assert old_lines == new_lines
 
-    # Commit car.py changes
-    subprocess.run(["git", "add", "src/car.py"])
-    subprocess.run(["git", "commit", "-m", "car_changes"])
+    # Commit changes
+    subprocess.run(["git", "add", filename])
+    subprocess.run(["git", "commit", "-m", "change1"])
 
     # Committed changes, run tests_selector
     # Should update db
     subprocess.run(["tests_selector"])
-    conn = sqlite3.connect(initial_mapping_name)
-    c = conn.cursor()
-    new_car_lines = [
-        x[0]
-        for x in c.execute(
-            "SELECT line_id FROM test_map WHERE file_id = ?", (file_id_car,)
-        ).fetchall()
-    ]
-    conn.close()
-    assert old_car_lines != new_car_lines
-    assert new_car_lines == [4, 5, 9, 12, 15]
+    new_lines = get_mapping_lines_for_srcfile(src_file_id)
+    assert old_lines != new_lines
+    assert new_lines == new_mapping
 
     # Run again, shouldn't update db
     subprocess.run(["tests_selector"])
-    conn = sqlite3.connect(initial_mapping_name)
-    c = conn.cursor()
-    new_car_lines = [
-        x[0]
-        for x in c.execute(
-            "SELECT line_id FROM test_map WHERE file_id = ?", (file_id_car,)
-        ).fetchall()
-    ]
-    conn.close()
-    assert new_car_lines == [4, 5, 9, 12, 15]
+    new_lines = get_mapping_lines_for_srcfile(src_file_id)
+    assert new_lines == new_mapping
