3c3101d605679332c98c02ca8d39bf08f7546c4c
guotin <eero.kauhanen@helsinki.fi>
2020-10-19 09:39:32 +0300

    Refactor test_git

--- tests_selector/tests/test_git.py
+++ tests_selector/tests/test_git.py
@@ -1,133 +1,130 @@
+import pytest
+import subprocess
 from tests_selector.utils import git
 
 
-def test_changed_files_current_no_change():
-    assert git.changed_files_current() == []
+@pytest.mark.parametrize(
+    "changes, expected",
+    [
+        ([("changes/car/shift_2_forward.txt", "src/car.py")], ["src/car.py"]),
+        ([], []),
+        (
+            [
+                ("changes/car/shift_2_forward.txt", "src/car.py"),
+                ("changes/shop/change_get_price.txt", "src/shop.py"),
+            ],
+            ["src/car.py", "src/shop.py"],
+        ),
+    ],
+)
+def test_changed_files_current(changes, expected):
+    for c in changes:
+        change = c[0]
+        filename = c[1]
+        subprocess.run(["cp", "-f", change, filename])
+    assert git.changed_files_current() == expected
 
 
-def test_changed_files_current_one_change():
-    with open("./src/car.py", "r") as f:
-        lines = f.readlines()
-        lines[6] = "abcd\n"
-
-    with open("./src/car.py", "w") as f:
-        for line in lines:
-            f.write(line)
-
-    assert git.changed_files_current() == ["src/car.py"]
-
-
-def test_get_test_lines_and_update_lines1():
-    with open("./fake_diff_data/1.txt", "r") as f:
+@pytest.mark.parametrize(
+    "diff_file, real_changed_lines, real_line_updates",
+    [
+        (
+            "fake_diff_data/1.txt",
+            [
+                23,
+                29,
+                30,
+                34,
+                35,
+                36,
+                38,
+                40,
+                50,
+                59,
+                67,
+                72,
+                83,
+                91,
+                100,
+                109,
+                119,
+                135,
+                145,
+                161,
+                171,
+                181,
+                190,
+                199,
+                210,
+                219,
+                228,
+                238,
+            ],
+            [
+                (23, 2),
+                (29, 4),
+                (30, 5),
+                (34, 6),
+                (35, 7),
+                (36, 8),
+                (38, 9),
+                (40, 10),
+                (50, 12),
+                (59, 13),
+                (67, 15),
+                (72, 16),
+                (83, 17),
+                (91, 18),
+                (100, 19),
+                (109, 20),
+                (119, 22),
+                (135, 23),
+                (145, 25),
+                (161, 26),
+                (171, 27),
+                (181, 28),
+                (190, 29),
+                (199, 30),
+                (210, 31),
+                (219, 32),
+                (228, 33),
+                (238, 34),
+            ],
+        ),
+        ("fake_diff_data/2.txt", [83, 240], [(83, 0), (240, 1)]),
+        ("fake_diff_data/3.txt", [16, 24], [(16, 3), (24, 4)]),
+    ],
+)
+def test_get_test_lines_and_update_lines_fake_change(
+    diff_file, real_changed_lines, real_line_updates
+):
+    with open(diff_file, "r") as f:
         diff = f.read()
 
-    lines, updates, _ = git.get_test_lines_and_update_lines(diff)
+    lines_changed, line_updates, _ = git.get_test_lines_and_update_lines(diff)
 
-    real_lines = [
-        23,
-        29,
-        30,
-        34,
-        35,
-        36,
-        38,
-        40,
-        50,
-        59,
-        67,
-        72,
-        83,
-        91,
-        100,
-        109,
-        119,
-        135,
-        145,
-        161,
-        171,
-        181,
-        190,
-        199,
-        210,
-        219,
-        228,
-        238,
-    ]
-
-    real_updates = [
-        (23, 2),
-        (29, 4),
-        (30, 5),
-        (34, 6),
-        (35, 7),
-        (36, 8),
-        (38, 9),
-        (40, 10),
-        (50, 12),
-        (59, 13),
-        (67, 15),
-        (72, 16),
-        (83, 17),
-        (91, 18),
-        (100, 19),
-        (109, 20),
-        (119, 22),
-        (135, 23),
-        (145, 25),
-        (161, 26),
-        (171, 27),
-        (181, 28),
-        (190, 29),
-        (199, 30),
-        (210, 31),
-        (219, 32),
-        (228, 33),
-        (238, 34),
-    ]
-
-    assert lines == real_lines
-    assert updates == real_updates
+    assert lines_changed == real_changed_lines
+    assert line_updates == real_line_updates
 
 
-def test_get_test_lines_and_update_lines2():
-    with open("./fake_diff_data/2.txt", "r") as f:
-        diff = f.read()
+@pytest.mark.parametrize(
+    "change, filename, real_changed_lines, real_line_updates",
+    [
+        (
+            "changes/car/line_shifts_middle.txt",
+            "src/car.py",
+            [11, 17],
+            [(11, 2), (17, 5)],
+        ),
+    ],
+)
+def test_get_test_lines_and_update_lines_real_change(
+    change, filename, real_changed_lines, real_line_updates
+):
+    subprocess.run(["cp", "-f", change, filename])
 
-    lines, updates, _ = git.get_test_lines_and_update_lines(diff)
-    real_lines = [83, 240]
-    real_updates = [(83, 0), (240, 1)]
+    diff = git.file_diff_data_current(filename)
+    changed_lines, line_updates, _ = git.get_test_lines_and_update_lines(diff)
 
-    assert lines == real_lines
-    assert updates == real_updates
-
-
-def test_get_test_lines_and_update_lines3():
-    with open("./fake_diff_data/3.txt", "r") as f:
-        diff = f.read()
-
-    lines, updates, _ = git.get_test_lines_and_update_lines(diff)
-    real_lines = [16, 24]
-    real_updates = [(16, 3), (24, 4)]
-
-    assert lines == real_lines
-    assert updates == real_updates
-
-
-def test_get_test_lines_and_update_lines4():
-    with open("./src/car.py", "r") as f:
-        lines = f.readlines()
-        lines[11] = lines[11].strip() + "+1\n"
-        lines[15] = lines[15] + "\n\n"
-        lines[21] = "        if True:\n"
-
-    with open("./src/car.py", "w") as f:
-        for line in lines:
-            f.write(line)
-
-    diff = git.file_diff_data_current("src/car.py", ".")
-    lines, updates, _ = git.get_test_lines_and_update_lines(diff)
-    real_lines = [12, 16, 22]
-    real_updates = [(12, 0), (16, 2), (22, 2)]
-
-    assert lines == real_lines
-    assert updates == real_updates
+    assert changed_lines == real_changed_lines
+    assert line_updates == real_line_updates
